612389a328e9af1bc5f24460bc1cde7bcd02caa4
Anna Beloshkurnik <squirrelpochta2@mail.ru>
2023-02-15 21:54:10 +0300

    Selenide

--- src/test/java/ru/netology/web/CardTest.java
+++ src/test/java/ru/netology/web/CardTest.java
@@ -1,50 +1,19 @@
 package ru.netology.web;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardTest {
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
 
     @Test
     void shouldTest() throws InterruptedException {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("input[name=\"name\"]")).sendKeys("Белошкурник Анна");
-        driver.findElement(By.cssSelector("input[name=\"phone\"]")).sendKeys("+79107651717");
-        driver.findElement(By.tagName("label")).click();
-        driver.findElement(By.cssSelector("button[type=\"button\"]")).click();
-        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("p[data-test-id=\"order-success\"]")).getText();
-        assertEquals(expected, actual);
+open("http://localhost:9999/");
+        $("input[name=\"name\"]").setValue("Белошкурник Анна");
+        $("input[name=\"phone\"]").setValue("+79107651717");
+        $("label").click();
+        $("button[type=\"button\"]").click();
+        $("p[data-test-id=\"order-success\"]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
