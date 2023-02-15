f442535e0e1dfaccff359c67dfa5477dec65a902
Anna Beloshkurnik <squirrelpochta2@mail.ru>
2023-02-15 20:17:05 +0300

    Add project

---
+++ src/test/java/ru/netology/web/CardTest.java
@@ -0,0 +1,49 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CardTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTest() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("input[name=\"name\"]")).sendKeys("Белошкурник Анна");
+        driver.findElement(By.cssSelector("input[name=\"phone\"]")).sendKeys("+79107651717");
+        driver.findElement(By.tagName("label")).click();
+        driver.findElement(By.cssSelector("button[type=\"button\"]")).click();
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("p[data-test-id=\"order-success\"]")).getText();
+        assertEquals(expected, actual);
+    }
+}
